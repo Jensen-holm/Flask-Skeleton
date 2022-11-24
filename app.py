ff275c1a23597223acab535811fde245f2e996d5
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-11-24 16:50:46 -0500

    datetime works

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
-def main() -> str:
-	return "Hello Flask"
+@app.route("/index")
+def main(date=None) -> str:
+	return render_template("index.html", date=datetime.now())
 
 
 if __name__ == "__main__":
